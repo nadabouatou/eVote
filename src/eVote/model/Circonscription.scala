5469d91aaee9815ac326fe4c43f13efc1d3847ce
ranto <ranto.andrianjohany@gmail.com>
2014-11-04 04:39:03 +0100

    Mis à jour du skeleton dans src

---
+++ src/eVote/model/Circonscription.scala
@@ -0,0 +1,6 @@
+package eVote.model
+
+class Circonscription {
+	def ajouterCanton(canton: Int,numeroCirconscription: Int):Unit={}
+	def supprimerCanton(codeCanton:Int):Unit={}
+}
---
+++ src/eVote/model/Region.scala
@@ -0,0 +1,7 @@
+package eVote.model
+
+class Region {
+	def ajouterRegion(pays: Int, codeRegion: Int, nom: String):Unit={}
+	def modifierRegion(codeRegion: Int,codePays: Int, nomRegion: String):Unit={}
+	def supprimerRegion(codeRegion: Int):Unit={}
+}
