005e06aafaab5b8e539552c78fbd55c0a4c7cd5c
merzouk <merzouk1985@gmail.com>
2014-11-12 09:48:12 +0100

    Singleton connect to DB

--- src/eVote/controler/DBConnexion.scala
+++ src/eVote/controler/DBConnexion.scala
@@ -1,7 +1,25 @@
 package eVote.controler
+import java.sql.DriverManager
+import java.sql.Connection
 
 //Pattern singleton pour la connexion à la BDD
 
 object DBConnexion {
-	
+    private var connection:Connection = null
+	def conn():Connection = {
+    // connect to the database
+    val driver = "org.postgresql.Driver"
+    val url = "jdbc:postgresql://localhost:5432/eVote"
+    val username = "postgres"
+    val password = "andrianjohany"
+ 
+    try {
+      // make the connection
+      Class.forName(driver)
+      connection = DriverManager.getConnection(url, username, password)
+    } catch {
+      case e => e.printStackTrace
+    }   
+    return connection
+  }
 }
