88d52fe71337f17103841eab735c408a6c18528b
mnabe <mohamedsalounabe@yahoo.fr>
2014-11-18 20:08:24 +0100

    Ajout de la classe Region

--- src/eVote/model/Region.scala
+++ src/eVote/model/Region.scala
@@ -1,7 +1,67 @@
 package eVote.model
 
+import eVote.controler.DBConnexion
+
 class Region {
-	def ajouterRegion(pays: Int, codeRegion: Int, nom: String):Unit={}
-	def modifierRegion(codeRegion: Int,codePays: Int, nomRegion: String):Unit={}
-	def supprimerRegion(codeRegion: Int):Unit={}
+  
+  // Pour ajouter dans la table region
+  def ajouterRegion(codePays: Int, codeRegion: Int, nom: String):Unit={
+    
+    val st = DBConnexion.conn().createStatement();
+    val req = "INSERT into region VALUES(" + codeRegion + "," + codePays + ",'" + nom + "')";
+    st.execute(req);
+    st.close();
+  }
+  
+  // pour supprimer dans la table region
+  def supprimerRegion(codeRegion: Int):Unit={
+    val st = DBConnexion.conn().createStatement();
+    val req="DELETE from region where codereg=" + codeRegion;
+    st.execute(req);
+    st.close();
+  }
+  
+  // pour modifier le code d'un pays dans la table region
+  def modifierCodePaysRegion(codeRegion: Int,codePays: Int):Unit={
+    
+    val st = DBConnexion.conn().createStatement();
+    val req="UPDATE region set codepays=" + codePays + "where codereg=" + codeRegion;
+    st.execute(req);
+    st.close();
+    
+  }
+  
+  // pour modifier le nom d'une region
+   def modifierNomRegion(codeRegion: Int,nom: String):Unit={
+    
+    val st = DBConnexion.conn().createStatement();
+    val req="UPDATE region set denommination='" + nom + "'where codereg=" + codeRegion;
+    st.execute(req);
+    st.close();
+    
+  }
+   
+   // pour modifier le code d'une region
+   def modifierCodeRegion(codeRegion: Int,nom: String):Unit={
+    
+    val st = DBConnexion.conn().createStatement();
+    val req="UPDATE region set codereg=" + codeRegion + " where denomination=" + nom;
+    st.execute(req);
+    st.close();
+    
+  }
+   
+   // pour modifier le code et le nom d'une region
+   def modifierRegion(codeRegion: Int, nomRegion: String):Unit={
+     
+    val st = DBConnexion.conn().createStatement();
+    val req="UPDATE region set codereg=" + codeRegion + ",denommination='" + nomRegion + "'where codereg=" + codeRegion;
+    st.execute(req);
+    st.close();
+     
+   }
+   
+   
+  
+
 }
